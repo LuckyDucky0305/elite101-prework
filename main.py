358054bf56374c475e7e81c5d6caf20aebc7c980
LuckyDucky0305 <caitlintze@gmail.com>
2025-01-15 01:04:32 +0000

    update: finish set up by having chatbot ask user what they'd like to do and adding placeholders for options.

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-print("Welcome to the craft store Chatbot")
+#initializing arrays for the inventory and the user's cart.
+inventory = []
+cart = []
+
+def action(choice):
+    if choice == "1":
+        print("Displaying inventory")
+    elif choice == "2":
+        print("Display shopping cart")
+    elif choice == "3":
+        print("Recommending you a craft related hobby")
+
+
+print("-----Welcome to Caitlin's Craft Store Chatbot-----")
 name = input("What is your name? ")
 age = input("Hello " + name + ", how old are you? ")
+
+while True:
+    print("---Caitlin's Craft Store---")
+    print("1. View the inventory")
+    print("2. Add to shopping cart")
+    print("3. Recommend me a craft related hobby")
+    print("4. Exit")
+    choice = input("How can I help? ")
+    if choice == "4":
+        print("Thank you! Please come again")
+        break
+    action(choice)
